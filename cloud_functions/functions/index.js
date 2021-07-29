1a6041014ed5b354d28853433a7d5ef2874b56e1
vitali gujevski <gujevski@gmail.com>
2021-07-29 23:39:49 +0300

    implemented cloud function to create new user in the database on first login

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -1,4 +1,6 @@
-import functions from "firebase-functions";
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +9,16 @@ import functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.addNewUserToDatabase = functions.auth.user().onCreate((user) => {
+  admin.initializeApp();
+  const newUser = {
+    uid: user.uid,
+    displayName: user.displayName,
+    email: user.email,
+  };
+  admin.firestore().collection("users").add(newUser).then((result) => {
+    functions.logger.info("new user", JSON.stringify(user, null, 2));
+    functions.logger.info(result);
+  });
+});
