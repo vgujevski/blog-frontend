62ded5b4abee69f51778af36a7eaa85bac24133c
vitali <gujevski@gmail.com>
2021-08-05 18:50:11 +0300

    removed firebase config

--- src/firebase-vars.js
+++ src/firebase-vars.js
@@ -1,12 +1,5 @@
 const firebaseVars = {
-  apiKey: "AIzaSyDRtLtfDEgIUOwaLRXwsWsWW6adkgLVsL4",
-  authDomain: "blog-i-2e93b.firebaseapp.com",
-  databaseURL: "https://blog-i-2e93b.firebaseio.com",
-  projectId: "blog-i-2e93b",
-  storageBucket: "blog-i-2e93b.appspot.com",
-  messagingSenderId: "143817251846",
-  appId: "1:143817251846:web:d1474dd47cfff369d53787",
-  measurementId: "G-QJFFS6V3G5"
+  // firebase config object
 }
 
 export default firebaseVars
