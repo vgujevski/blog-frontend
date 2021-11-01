d975f0dfed712d371a94f9192590438cebed6482
Vitali G <gujevski@gmail.com>
2021-11-01 20:18:09 +0200

    feature: add delete comment button to Comment component

--- src/features/comments/Comment.js
+++ src/features/comments/Comment.js
@@ -4,18 +4,33 @@ import PropTypes from 'prop-types'
 import { DateComponent } from '../posts/Date'
 
 import { selectCommentById } from './commentsSlice'
-import { selectUserById } from '../users/usersSlice' 
+import { selectUserById } from '../users/usersSlice'
+import { selectLoggedInUser } from '../auth/authSlice'
 
-export const Comment = ({commentId}) => {
+export const Comment = ({ commentId }) => {
 
   const { author: authorId, commentedOn, content } = useSelector(state => selectCommentById(state, commentId))
   const { displayName } = useSelector(state => selectUserById(state, authorId))
+  const loggedInUser = useSelector(selectLoggedInUser)
+
+  const onDeleteClicked = () => {
+    alert('delete clicked')
+  }
+
+  const renderDeleteButton = () => {
+    if (loggedInUser && loggedInUser.uid === authorId) {
+      return (
+        <button className="button btn-main" onClick={onDeleteClicked}>Delete Comment</button>
+      )
+    }
+  }
 
   return (
     <div>
       <h3>{content}</h3>
       <p>by {displayName}</p>
-      <DateComponent date={commentedOn}/>
+      <DateComponent date={commentedOn} />
+      {renderDeleteButton()}
     </div>
   )
 }
