d7ede1bc493933e420cc4fcfb20e71340a12a03e
Vitali G <gujevski@gmail.com>
2021-11-06 15:09:32 +0200

    styling: adjust styles for LoginButton

--- src/features/auth/LoginButton.js
+++ src/features/auth/LoginButton.js
@@ -1,30 +1,32 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { startLogin, startLogout } from './authentication'
-
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { startLogin, startLogout } from "./authentication";
 
 export const LoginButton = () => {
-  const user = useSelector(state => state.auth.user)
+  const user = useSelector((state) => state.auth.user);
 
   const handleLoginClick = () => {
-    startLogin()
-  }
+    startLogin();
+  };
 
   const handleLogoutClick = () => {
-    startLogout()
-  }
-  const renderButton = () => (
-    user ?
-      <button className="button btn-main" onClick={handleLogoutClick}>Logout</button>
-      :
-      <button className="button btn-main" onClick={handleLoginClick}>Login</button>
-  )
+    startLogout();
+  };
+  const renderButton = () =>
+    user ? (
+      <button className="button btn-main" onClick={handleLogoutClick}>
+        Logout
+      </button>
+    ) : (
+      <button className="button btn-main" onClick={handleLoginClick}>
+        Login
+      </button>
+    );
 
   return (
-    <div>
+    <div className="auth-container">
+      {user && <p>Hi, {user.displayName}</p>}
       {renderButton()}
-      {user && <div>Welcome {user.displayName}</div>}
     </div>
-  )
-}
+  );
+};
